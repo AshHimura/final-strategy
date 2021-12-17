a4a7b484d492d496172a5d06f07120d57a35585b
Chris Mccrary <chris.mccrary@asurion.com>
2021-12-17 14:59:49 -0600

    Created sound start and stup but dang navbar reared its ugly head again

--- src/components/landing/SplashPage.js
+++ src/components/landing/SplashPage.js
@@ -1,42 +1,61 @@
 import React, { useState, useEffect } from 'react'
+import useSound from 'use-sound'
+import intro from '../music/intro.mp3'
 import { Link } from "react-router-dom";
 import './SplashPage.css'
 import '../../index.css'
 
 export const SplashPage = () => {
+    const [showPage, setShowPage] = useState(false)
     const [games, setGame] = useState([])
-    const [user, setUser] = useState([])
-
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [play, {stop}] = useSound(intro, {volume: 0.08})
+    
     useEffect(
         () => {
             fetch("http://localhost:8088/games")
-                .then(res => res.json())
-                .then((data) => {
-                    setGame(data)
-                })
+            .then(res => res.json())
+            .then((data) => {
+                setGame(data)
+            })
         }, [])
 
+    const playSong = () => {
+        setIsPlaying(true)
+        play()
+    }
+
+    const stopSong = () => {
+        setIsPlaying(false)
+        stop()
+    }
     return (
         <>
             <div className="container">
+                <button onClick={()=>{setShowPage(!showPage); playSong()}}>Welcome</button>
 
-                <li className="splashbar__item active">
-                    <Link className="splashbar__link" to="#" onClick={
-                        () => {
-                            localStorage.removeItem("strategy_user")
-                        }
-                    }>Logout!</Link>
+                {showPage ? <div> 
+            <li className="splashbar__item active">
+            <Link className="splashbar__link" to="#" onClick={
+                () => {
+                    localStorage.removeItem("strategy_user")
+                }
+            }>Logout!</Link>
                 </li>
 
                 <h1>Final Strategy</h1>
                 <div> 
                     {
-                    games.map((gameObj) => {
-                        return <Link to={`/game/${parseInt(gameObj.id)}`} id={gameObj.id}> 
+                        games.map((gameObj) => {
+                            return <Link to={`/game/${parseInt(gameObj.id)}`} id={gameObj.id}> 
                             <img key={gameObj.id} src={`http://localhost:8080/${gameObj.img}`} alt={gameObj.alt} />
                         </Link>
                     })}
-                    
+                    </div>
+                    </div> :null}
+
+                    <div>
+                    <button onClick={isPlaying ? stopSong : playSong}>Stop Music</button>
                     </div>
 
             </div>
